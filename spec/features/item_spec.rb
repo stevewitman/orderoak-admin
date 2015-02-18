9a706410e3cb32517969eb51d80566b190f28cd6
Steve Witman <stevewitman@gmail.com>
2015-02-18 14:28:14 -0700

    added new item spec

--- spec/features/item_spec.rb
+++ spec/features/item_spec.rb
@@ -1,1 +1,19 @@
-require ‘rails_helper’
+require "rails_helper"
+
+feature "Items" do
+
+  scenario "Visitor can add menu item" do
+    visit '/'
+    click_on "New Menu Item"
+    fill_in "Name", with: "TestName"
+    fill_in "Description", with: "TestDescription"
+    fill_in "Price", with: "1000"
+    check('In stock')
+    click_on "Create Item"
+    expect(page).to have_content("TestName")
+    expect(page).to have_content("TestDescription")
+    expect(page).to have_content("1000")
+    expect(page).to have_content("true")
+  end
+
+end
