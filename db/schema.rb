2926f2c5d046fabb9a7b5e88011a5f246fc6ca3f
Steve Witman <stevewitman@gmail.com>
2015-03-23 10:42:06 -0600

    merged api branch

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150321194306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
+  create_table "items", force: :cascade do |t|  
     t.string  "name"
     t.text    "description"
     t.integer "price"
